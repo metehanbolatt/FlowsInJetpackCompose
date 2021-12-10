158e76fc767eacb6035ede0e28ee66b605b4ab50
metehanie <metehan.bolat.ie@gmail.com>
2021-12-10 17:34:24 +0300

    countDownFlow.filter - viewModelScope.launch

--- app/src/main/java/com/metehanbolat/flowsinjetpackcompose/MainViewModel.kt
+++ app/src/main/java/com/metehanbolat/flowsinjetpackcompose/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
@@ -26,7 +27,11 @@ class MainViewModel: ViewModel() {
 
     private fun collectFlow(){
         viewModelScope.launch {
-            countDownFlow.collect { time ->
+            countDownFlow
+                .filter { time ->
+                    time % 2 == 0
+                }
+                .collect { time ->
                 println("The current time is $time")
             }
         }

7d17cbc184461dbd6095f8c7591f2194129a5185
metehanie <metehan.bolat.ie@gmail.com>
2021-12-13 23:04:03 +0300

    collectLatestLifecycleFlow

--- app/src/main/java/com/metehanbolat/flowsinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/flowsinjetpackcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.metehanbolat.flowsinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
@@ -11,12 +12,24 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.metehanbolat.flowsinjetpackcompose.ui.theme.FlowsInJetpackComposeTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        collectLatestLifecycleFlow(viewModel.stateFlow) { number ->
+            // You can use number here.
+        }
         setContent {
             FlowsInJetpackComposeTheme {
                 val viewModel = viewModel<MainViewModel>()
@@ -27,6 +40,14 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+        }
+    }
+}
+
+fun <T> ComponentActivity.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit){
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED){
+            flow.collectLatest(collect)
         }
     }
 }
